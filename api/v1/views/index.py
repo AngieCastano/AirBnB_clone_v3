c5014bca4c08351b7dbc58aac3b29de3e6d708cc
Santiago-Gallego <santiagoxd68@hotmail.com>
2020-09-09 17:33:13 +0000

    [Fix]pycodestyle

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,10 +4,12 @@ from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
+
 @app_views.route('/status')
 def status():
     """ Return http status response """
     return jsonify({'status': 'OK'})
+
 
 @app_views.route('/stats')
 def get_stats():
