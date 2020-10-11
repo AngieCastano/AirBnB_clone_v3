c34d2720187c2860e5b093017871e60c85d31328
AngieCastano1634 <1634@holbertonschool.com>
2020-10-11 04:43:01 +0000

    [ADD][New function to use mysqldb to select SELECT place_id FROM place_amenity WHERE amenity_id = {}]

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -94,3 +94,10 @@ class DBStorage:
     def count(self, cls=None):
         """ Returns the number of stored objects that match the class name """
         return len(self.all(cls))
+
+    def places_amenities(self, amenity_id=None):
+        """ Return every place with amenity_id from the place_amenity table"""
+        return_from_mysql = self.__engine.execute("SELECT place_id FROM place_amenity WHERE amenity_id = \"{}\"".format(amenity_id))
+        places_with_same_amenity = return_from_mysql.cursor._rows
+        print(type(places_with_same_amenity[0][0]))
+        return places_with_same_amenity
